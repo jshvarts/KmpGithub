d079b8f89d51465287d28d5e92549261dc93f81f
James Shvarts <jshvarts@gmail.com>
2020-05-06 21:16:39 -0400

    Add ktor client and coroutines

---
+++ shared/src/androidMain/kotlin/api/GithubApi.kt
@@ -0,0 +1,15 @@
+package com.jshvarts.kmp.shared.api
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.http.Url
+
+class GithubApi {
+  private val client = HttpClient()
+
+  private val memberUrl = Url("http://api.github.com/orgs/squarespace/members")
+
+  suspend fun getMembers(): String {
+    return client.get(memberUrl.toString())
+  }
+}
